e1c4c5b1e9bec428480212fd60d3278283fee16f
Ilia <pops375@yandex.ru>
2023-04-20 20:40:23 +0300

    fix auth property

--- HCL.ArticleService.API/DIManger.cs
+++ HCL.ArticleService.API/DIManger.cs
@@ -6,7 +6,10 @@ using HCL.ArticleService.API.Domain.DTO;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.OData;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OData.ModelBuilder;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace HCL.ArticleService.API
 {
@@ -47,8 +50,32 @@ namespace HCL.ArticleService.API
 
         public static void AddAuthProperty(this WebApplicationBuilder webApplicationBuilder)
         {
-            webApplicationBuilder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options => webApplicationBuilder.Configuration.Bind("JwtSettings", options));
+            var secretKey = webApplicationBuilder.Configuration.GetSection("JWTSettings:SecretKey").Value;
+            var issuer = webApplicationBuilder.Configuration.GetSection("JWTSettings:Issuer").Value;
+            var audience = webApplicationBuilder.Configuration.GetSection("JWTSettings:Audience").Value;
+            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+            webApplicationBuilder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+
+                options.RequireHttpsMetadata = true;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = issuer,
+                    ValidateAudience = true,
+                    ValidAudience = audience,
+                    ValidateLifetime = true,
+                    IssuerSigningKey = signingKey,
+                    ValidateIssuerSigningKey = true,
+                };
+            });
         }
     }
 }
